e3e08061c0ff15acb41d65e1c3a450b8217c6de9
Pat <patrycja.kruczynska@gmail.com>
2026-09-01 09:57:43 +0200

    initialize sprint 1 patient

---
+++ src/test/java/com/medilabo/med/MedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.med;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
